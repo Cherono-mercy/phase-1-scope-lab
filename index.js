ff73e5e7ec3b036ef5a306996f0ed93e1e531d80
Mercy Cherono <cheronom142@gmail.com>
2023-09-21 23:44:31 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Declaring a variable in global scope
+var customerName = "bob";
+const leastFavoriteCustomer = "Mary";
+
+//Function that accesses global variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+//Declare a global variable inside a function
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+//Overwriting the global variable
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Error trying to reassign a constant variable declared in global scope
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Jane";
+}
